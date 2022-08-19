d6e0694f601437eec6526795f947143be93e123e
Remix Workspace <>
2022-08-19 06:02:38 +0100

    Implemented withdraw fund

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -6,16 +6,50 @@ import "./library/PriceConverter.sol";
 contract Transaction {
     using PriceConverter for uint256;
 
-    uint256 public mininumUSD = 2 * 1e18;
+    uint256 public minimumUSD = 2 * 1e18;
     address[] public funder;
     mapping(address => uint256) public amountFunded;
+    address public owner;
+
+    // Set the owner of the contract
+    constructor() {
+        owner = msg.sender;
+    }
+
 
     function fund() public payable {
-        require(msg.value.converter() >= mininumUSD, "Amount must be greater than 1");
+        require(msg.value.converter() >= minimumUSD, "Amount must be greater than 1");
         funder.push(msg.sender);
         amountFunded[msg.sender] = msg.value;
 
     } 
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funder.length; funderIndex++) {
+            address funderAddress = funder[funderIndex];
+            amountFunded[funderAddress] = 0;
+        }
+
+        // Reset the funder array
+        funder = new address[](0);
+
+        // // Perform withdraw here
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool snd = payable(msg.sender).send(address(this).balance);
+        // require(snd, "Amount can not be without");
+
+        // call
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Review amount");
+    }
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can access this");
+        _;
+    }
 
 
 }
